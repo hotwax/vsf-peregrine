40449d6bf6675cf4075d54b11b5cc2cebeeccd85
Yash Maheshwari <yashmaheshwari551@gmail.com>
2020-05-22 14:57:45 +0530

    Updated: the code for registering the Peregrine CMS module according to the Storefront v1.11

--- index.ts
+++ index.ts
@@ -1,14 +1,19 @@
-import { createModule } from '@vue-storefront/core/lib/module'
+// import { createModule } from '@vue-storefront/core/lib/module'
 import { module } from './store'
 import { beforeRegistration } from './hooks/beforeRegistration'
 import { afterRegistration } from './hooks/afterRegistration'
 import { beforeEach } from './router/beforeEach'
+import { StorefrontModule } from '@vue-storefront/core/lib/modules'
 
-const KEY = 'cmsstore'
-export const PeregrineStore = createModule({
-  key: KEY,
-  store: { modules: [{ key: KEY, module }] },
-  beforeRegistration,
-  afterRegistration,
-  router: { beforeEach }
-})
+export const KEY = 'cmsstore'
+
+export const PeregrineStore: StorefrontModule = function ({app, store, router, moduleConfig, appConfig}) {
+  store.registerModule(KEY, module)
+  router.beforeEach(beforeEach)
+  beforeRegistration.afterAppInit(() => {
+    console.log('beforeRegistration hook registered...')
+  })
+  afterRegistration.afterAppInit(() => {
+    console.log('afterRegistration hook registered...')
+  })
+}
