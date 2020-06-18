92eec5381fff85399d578a6b8e830873d93d7e35
Swapnil M Mane <swapnil.mane@hotwaxsystems.com>
2020-06-18 18:21:28 +0530

    Implemented Text Block using Storefront UI (3bpy9t)

---
+++ components/organisms/o-text-block.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <SfHeading :level="2" :title="componentData.title" />
+    <div v-html="componentData.text" />
+  </div>
+</template>
+
+<script>
+import { SfHeading } from "@storefront-ui/vue";
+
+export default {
+  components: {
+    SfHeading
+  },
+  props: {
+    componentData: {
+      type: Object
+    }
+  }
+}
+</script>
