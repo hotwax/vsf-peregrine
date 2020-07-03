e019b1c532697e14325a5893ad0fca74b3af9b14
Swapnil M Mane <swapnil.mane@hotwaxsystems.com>
2020-07-03 20:21:54 +0530

    Implemented New Arrival and Best Seller CMS widget (3hqk1m)

--- components/CmsPage.vue
+++ components/CmsPage.vue
@@ -17,6 +17,7 @@ const Newsletter = () => import('./organisms/o-newsletter')
 const Banner = () => import('./molecules/m-banner')
 const BannerGrid = () => import('./organisms/o-banner-grid')
 const BannerGridT = () => import('./organisms/o-banner-grid-t')
+const NewBestSeller = () => import('./organisms/o-new-best-seller')
 
 export default {
   props: {
@@ -33,7 +34,9 @@ export default {
         'bannerright': Banner,
         'verticalbannergrid': BannerGrid,
         'verticalset': BannerGrid,
-        'tgrid': BannerGridT
+        'tgrid': BannerGridT,
+        'bestseller': NewBestSeller,
+        'newarrivalproducts': NewBestSeller
       }
     };
   }
