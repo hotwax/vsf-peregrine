6b7cc6babb9038809d173975c83704446781ee0c
Aishwary Shrivastava <aishwary.shrivastava@hotwaxsystems.com>
2020-09-04 18:34:02 +0530

    Improved: Removed max-width styling and used the global variable

--- pages/Home.vue
+++ pages/Home.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="home" v-if="cmsComponents">
+  <div id="home" class="global--max-width" v-if="cmsComponents">
     <div>
       <CmsPage :cms-object="cmsComponents" />
     </div>
@@ -72,7 +72,7 @@ export default {
   box-sizing: border-box;
   padding: 0 var(--spacer-sm);
   @include for-desktop {
-    max-width: 1272px;
+    padding: 0 var(--spacer-sm);
     margin: 0 auto;
   }
 }
