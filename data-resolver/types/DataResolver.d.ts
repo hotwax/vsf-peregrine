d7154dbb0e10ec6a1b8d7f52431978a5de52065a
Deepak Dixit <deepak.dixit@hotwax.co>
2020-11-03 17:43:51 +0530

    Revert "Updated the code to implement the offline functionality for the stati…"

--- data-resolver/types/DataResolver.d.ts
+++
@@ -1,9 +0,0 @@
-import Task from '@vue-storefront/core/lib/sync/types/Task'
-
-export namespace DataResolver {
-
-    interface CmsService {
-        fetchCmsData: (url, actionName: string) => Promise<Task>
-        queueCmsData: (url, actionName: string) => Promise<Task>
-    }
-}
