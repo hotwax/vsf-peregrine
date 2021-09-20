20fa0b8bf968a25e4817d83c01b49eb6d63ff5a7
Yash Maheshwari <yash.maheshwari@hotwax.co>
2021-09-20 18:42:54 +0530

    Updated: the routes array to include the two new static pages entries(#chyxf6)

--- router/beforeEach.ts
+++ router/beforeEach.ts
@@ -5,7 +5,7 @@ import { currentStoreView } from '@vue-storefront/core/lib/multistore'
 /**
  * Add Route's name in peregrineRoutes whose data will come from peregrine cms
  */
-const peregrineRoutes = ['home', 'returns', 'size-guide', 'about-us', 'contact', 'privacy', 'legal', 'customer-service', 'delivery']
+const peregrineRoutes = ['home', 'returns', 'size-guide', 'about-us', 'contact', 'privacy', 'legal', 'customer-service', 'delivery', 'terms-of-service', 'shipping-policy']
 export async function beforeEach (to: Route, from: Route, next) {
   let routeTo = to.name
   let storeView = currentStoreView()
