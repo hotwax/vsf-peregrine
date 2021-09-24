900c80f42ea04df4497ef2d86277c63f689f1ef3
Yash Maheshwari <yash.maheshwari@hotwax.co>
2021-09-24 14:18:16 +0530

    Updated: code for banner-grid-t component to directly include the scss from storefront ui(#cx5jd7)

--- components/organisms/o-banner-grid-t.vue
+++ components/organisms/o-banner-grid-t.vue
@@ -90,6 +90,7 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+@import "~@storefront-ui/shared/styles/helpers";
 @import "~@storefront-ui/shared/styles/helpers/breakpoints";
 
 .banner-grid {
@@ -99,15 +100,163 @@ export default {
   }
 }
 
+//  copied styling of sf-banner-grid and sf-banner from storefront ui
+.sf-banner {
+  box-sizing: border-box;
+  display: var(--banner-display, flex);
+  justify-content: var(--banner-justify-content, flex-start);
+  width: var(--banner-width, 100%);
+  min-height: var(--banner-height, 21.4375rem);
+  @include background(
+    --banner-background,
+    var(--_banner-background-color, transparent),
+    var(--_banner-background-image)
+  );
+  --banner-background-position: 60%;
+  &__wrapper {
+    display: flex;
+    flex-direction: column;
+    align-items: var(--banner-align-items, flex-start);
+    justify-content: var(--banner-wrapper-justify-content, flex-start);
+    flex-direction: var(--banner-wrapper-flex-direction, column);
+    padding: var(--banner-padding, var(--spacer-xl));
+    flex: 0 0 var(--banner-wrapper-width, 100%);
+    text-decoration: none;
+    &.sf-button,
+    &.sf-button:hover,
+    &.sf-button:active {
+      --button-background: transparent;
+      --button-box-shadow-opacity: 0;
+    }
+  }
+  &__subtitle {
+    margin: var(--banner-subtitle-margin, 0);
+    color: var(
+      --banner-color,
+      var(--banner-subtitle-color, var(--c-dark-variant))
+    );
+    @include font(
+      --banner-subtitle-font,
+      var(--font-weight--normal),
+      var(--font-size--base),
+      1.4,
+      var(--font-family--secondary)
+    );
+    text-transform: var(--banner-subtitle-text-transform, uppercase);
+  }
+  &__title {
+    margin: var(--banner-title-margin, var(--spacer-2xs) 0 0 0);
+    color: var(--banner-color, var(--banner-title-color, var(--c-text)));
+    @include font(
+      --banner-title-font,
+      var(--font-weight--normal),
+      var(--h2-font-size),
+      1.4,
+      var(--font-family--secondary)
+    );
+    text-transform: var(--banner-title-text-transform, uppercase);
+  }
+  &__description {
+    display: var(--banner-description-display, none);
+    margin: var(
+      --banner-description-margin,
+      var(--spacer-sm) 0 var(--spacer-base) 0
+    );
+    color: var(--banner-color, var(--banner-description-color, var(--c-text)));
+    text-align: var(--banner-description-text-align, left);
+    @include font(
+      --banner-description-font,
+      var(--font-weight--light),
+      var(--font-size--base),
+      1.6,
+      var(--font-family--primary)
+    );
+  }
+  &__call-to-action {
+    --button-color: var(--c-light-variant);
+    display: var(--banner-display-call-to-action, none);
+  }
+  @include for-desktop {
+    --banner-background-image: var(
+      --_banner-background-desktop-image,
+      var(--_banner-background-image)
+    );
+    --banner-wrapper-width: 50%;
+    --banner-description-display: block;
+    --banner-display-call-to-action: block;
+    --banner-title-margin: var(--spacer-sm) 0 0 0;
+    --banner-subtitle-color: var(--c-gray);
+    align-items: var(--banner-align-items, center);
+    min-height: var(--banner-height, 25rem);
+    &__wrapper {
+      pointer-events: none;
+    }
+    &__call-to-action {
+      --button-padding: var(--spacer-sm) var(--spacer-xl);
+      pointer-events: auto;
+    }
+    &--right {
+      --banner-justify-content: flex-end;
+    }
+  }
+}
+
 .sf-banner-grid {
+  --banner-align-items: flex-start;
   --banner-container-width: 50%;
+  --banner-description-margin: var(--spacer-xl) 0;
   &__row {
+    display: var(--banner-grid-row-display, block);
+    flex: var(--banner-grid-row-flex);
+    margin: var(--banner-grid-row-margin, 0);
     & + & {
       --banner-grid-row-margin: var(--spacer-sm) 0 0 0;
     }
     @media (min-width: $desktop-min) {
       & + & {
         --banner-grid-row-margin: var(--spacer-xl) 0 0 0;
+      }
+    }
+  }
+  &__col {
+    display: var(--banner-grid-col-display, block);
+    flex: var(--banner-grid-col-flex);
+    margin: var(--banner-grid-col-margin, 0);
+    & + & {
+      --banner-grid-col-margin: var(--spacer-sm) 0 0 0;
+    }
+  }
+  @include for-desktop {
+    --banner-grid-row-display: flex;
+    --banner-grid-row-flex: 0 0 100%;
+    --banner-grid-col-display: flex;
+    &__row {
+      --banner-grid-row-margin: 0;
+      & + & {
+        --banner-grid-row-margin: var(--spacer-xl) 0 0 0;
+      }
+    }
+    &__col {
+      --banner-grid-col-flex: 1;
+      flex-wrap: wrap;
+      --banner-grid-col-margin: 0;
+      & + & {
+        --banner-grid-col-margin: 0 0 0 var(--spacer-xl);
+      }
+      & > .sf-banner,
+      & > a {
+        display: flex;
+        flex: 1;
+      }
+      &--small {
+        --banner-grid-col-flex: 0 1 36%;
+      }
+      &--medium {
+        --banner-grid-col-flex: 0 1 40%;
+        --banner-container-width: 70%;
+      }
+      &--higher {
+        --banner-height: 510px;
       }
     }
   }
